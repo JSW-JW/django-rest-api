9b7a94b014ce7f6b573788a830ace2ac70bbf077
Seungwon <jshtmddnjs@naver.com>
2021-05-20 19:38:15 +0900

    json.dumps() and is_json validation + formValidation

--- _scripts/cfe_pure_api.py
+++ _scripts/cfe_pure_api.py
@@ -1,6 +1,5 @@
 import json
-import requests # http requests
-
+import requests  # http requests
 
 BASE_URL = "http://127.0.0.1:8000/"
 ENDPOINT = "api/updates/"
@@ -10,7 +9,7 @@ def get_list(): #--> Lists all this out
     r = requests.get(BASE_URL + ENDPOINT)
     print(r.status_code)
     status_code = r.status_code
-    if status_code != 200: # not found
+    if status_code != 200:
         print('probably not good sign?')
     data = r.json()
     #print(type(json.dumps(data)))
@@ -26,16 +25,52 @@ def get_list(): #--> Lists all this out
 def create_update():
     new_data = {
         'user': 1,
-        "content": "Another new cool update"
+        "content": "validate create_update content"
     }
-    r = requests.post(BASE_URL + ENDPOINT, data=new_data)
+
+    r = requests.post(BASE_URL + ENDPOINT, data=json.dumps(new_data))
+
     print(r.headers)
     print(r.status_code)
     if r.status_code == requests.codes.ok:
-        #print(r.json())
         return r.json()
     return r.text
 
-#print(get_list())
+
+def create_update_not_allowed():
+    data_empty = {
+        'user': '1',
+        'content': '',
+    }
+
+    r = requests.post(BASE_URL + ENDPOINT + "1/", data=json.dumps(data_empty))
+    print(r.headers)
+    print(r.status_code)
+    if r.status_code == requests.codes.ok:
+        return r.json()
+    return r.text
+
+
+def do_obj_update():
+    new_data = {
+        'user': 1,
+        "content": "updated"
+    }
+    r = requests.put(BASE_URL + ENDPOINT + "1/", data=json.dumps(new_data))
+
+    print(r.status_code)
+    if r.status_code == requests.codes.ok:
+        return r.json()
+    return r.text
+
+
+def do_obj_delete():
+    r = requests.delete(BASE_URL + ENDPOINT + "1/")
+
+    print(r.status_code)
+    if r.status_code == requests.codes.ok:
+        return r.json()
+    return r.text
+
 
 print(create_update())
