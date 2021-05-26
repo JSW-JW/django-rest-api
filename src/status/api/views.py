f363aee25c77f01fd3eb51c16a7bf689327fd94e
Seungwon <jshtmddnjs@naver.com>
2021-05-26 21:48:22 +0900

    get_object

--- src/status/api/views.py
+++ src/status/api/views.py
@@ -9,6 +9,8 @@ from django.shortcuts import get_object_or_404
 from .serializers import StatusSerializer
 from ..models import Status
 
+import json
+
 
 # class StatusListSearchAPIView(APIView):
 #     permission_classes = []
@@ -22,6 +24,14 @@ from ..models import Status
 #     def post(self, request, format=None):
 #         pass
 
+def is_json(json_data):
+    try:
+        real_json = json.loads(json_data)
+        is_valid = True
+    except ValueError:
+        is_valid = False
+    return is_valid
+
 
 class StatusAPIView(mixins.CreateModelMixin,
                     mixins.RetrieveModelMixin,
@@ -32,6 +42,7 @@ class StatusAPIView(mixins.CreateModelMixin,
     authentication_classes = []
     queryset = Status.objects.all()
     serializer_class = StatusSerializer
+    passed_id = None
 
     def get_queryset(self):
         qs = Status.objects.all()
@@ -40,19 +51,34 @@ class StatusAPIView(mixins.CreateModelMixin,
             qs = qs.filter(content__icontains=query)
         return qs
 
+    # def get_object(self):
+    #     request = self.request
+    #     passed_id = request.GET.get('id', None) or self.passed_id
+    #     queryset = self.get_queryset()
+    #     obj = None
+    #     if passed_id is not None:
+    #         obj = get_object_or_404(queryset, id=passed_id)
+    #         self.check_object_permissions(request, obj)
+    #     return obj
+
     def get_object(self):
-        request = self.request
-        passed_id = request.GET.get('id', None)
+        id = json.loads(self.request.body).get('id')
         queryset = self.get_queryset()
-        obj = None
-        if passed_id is not None:
-            obj = get_object_or_404(queryset, id=passed_id)
-            self.check_object_permissions(request, obj)
+
+        obj = get_object_or_404(queryset, id=id)
         return obj
 
-    def get(self, *args, **kwargs): # overrides 'get' method from ListAPIView
-        request = self.request
-        passed_id = request.GET.get('id', None)
+    def get(self, request, *args, **kwargs): # overrides 'get' method from ListAPIView
+        url_passed_id = request.GET.get('id', None)
+        json_data = {}
+        body_ = request.body
+        print(request.body)
+        if is_json(body_):
+            json_data = json.loads(request.body)
+        new_passed_id = json_data.get('id', None)
+
+        passed_id = url_passed_id or new_passed_id or None
+        self.passed_id = passed_id
         if passed_id is not None:
             return self.retrieve(request, *args, **kwargs)
         return super().get(request, *args, **kwargs)
@@ -61,12 +87,45 @@ class StatusAPIView(mixins.CreateModelMixin,
         return self.create(request, *args, **kwargs)
 
     def put(self, request, *args, **kwargs):
+        url_passed_id = request.GET.get('id', None)
+        json_data = {}
+        body_ = request.body
+        print(request.body)
+        if is_json(body_):
+            json_data = json.loads(request.body)
+        new_passed_id = json_data.get('id', None)
+
+        passed_id = url_passed_id or new_passed_id or None
+        self.passed_id = passed_id
+
         return self.update(request, *args, **kwargs)
 
     def patch(self, request, *args, **kwargs):
+        url_passed_id = request.GET.get('id', None)
+        json_data = {}
+        body_ = request.body
+        print(request.body)
+        if is_json(body_):
+            json_data = json.loads(request.body)
+        new_passed_id = json_data.get('id', None)
+
+        passed_id = url_passed_id or new_passed_id or None
+        self.passed_id = passed_id
+
         return self.update(request, *args, **kwargs)
 
     def delete(self, request, *args, **kwargs):
+        url_passed_id = request.GET.get('id', None)
+        json_data = {}
+        body_ = request.body
+        print(request.body)
+        if is_json(body_):
+            json_data = json.loads(request.body)
+        new_passed_id = json_data.get('id', None)
+
+        passed_id = url_passed_id or new_passed_id or None
+        self.passed_id = passed_id
+
         return self.destroy(request, *args, **kwargs)
 
 
